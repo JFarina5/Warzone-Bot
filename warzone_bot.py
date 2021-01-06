e9533e1f7fbae8b10a2492d859bf88b53fd7df86
John Michael Farina <john.farina0824@gmail.com>
2021-01-05 23:04:17 -0500

    Update warzone_bot.py

--- warzone_bot.py
+++ warzone_bot.py
@@ -11,14 +11,16 @@ load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 bot = commands.Bot(command_prefix='!')
 
+# Test to ensure warzone bot has successfully connected to Discord
 @bot.event
 async def on_ready():
     print(f'{bot.user.name} has connected to Discord!')
 
+# Drop location method which will randomly display a location, based from the provided text file
 @bot.command(name='wherewedropping', help='When in doubt, have Warzone Bot help you out; randomly chooses a drop point for you and/or your squad in Verdansk.')
 async def drop_location(ctx):
-    lines = open('verdansk_locations.txt').read().splitlines()
-    myline = random.choice(lines)
+    lines = open('verdansk_locations.txt').read().splitlines() # Opens txt document and reads eachline
+    myline = random.choice(lines) # Randomizes the results in the 'lines' variable
 
     drop_response = [
         (myline + ' looks like a good place to drop.'), 
@@ -26,7 +28,7 @@ async def drop_location(ctx):
         ('I\'d reccomend dropping at ' + myline)
     ]
 
-    response = random.choice(drop_response)
+    response = random.choice(drop_response) # Used to randomize the response provided by the bot, based on the 'drop_response' variable
 
     await ctx.send(response)
 
